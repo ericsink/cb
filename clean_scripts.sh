615172f18b67db59e3075f757be016d7945dc11a
Eric Sink <eric@sourcegear.com>
2018-10-17 16:02:59 -0500

    initial code moved over from SQLitePCL.raw repo

---
+++ clean_scripts.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+rm ./*.linkargs
+rm ./*.gccargs
+rm ./*.libtoolfiles
+rm ./win_*.bat
+rm ./*.buildoutput.txt
+rm ./linux_*.sh
+rm ./android_*.sh
+rm ./ios_*.sh
+rm ./mac_*.sh
+#rm -rf ./obj
+#rm -rf ./bin
+
