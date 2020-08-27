47d5bd76b204bda2bab9007d45aa59e0471aa246
Eric Sink <eric@sourcegear.com>
2020-08-27 13:27:01 -0500

    setup_linux.sh

---
+++ bld/setup_linux.sh
@@ -0,0 +1,6 @@
+#1/bin/sh
+sudo apt-get install gcc-arm-linux-gnueabi
+sudo apt-get install gcc-arm-linux-gnueabihf
+sudo apt-get install musl-dev musl-tools
+sudo apt-get install gcc-aarch64-linux-gnu
+
