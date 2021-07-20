a0604fa60ea21f283678546b9cc3bbd4d50440ec
Ulrich Weigand <ulrich.weigand@de.ibm.com>
2021-07-20 13:42:26 +0000

    Add support for linux-s390x target

--- bld/setup_linux.sh
+++ bld/setup_linux.sh
@@ -10,3 +10,4 @@ sudo apt-get install gcc-arm-linux-gnueabihf
 sudo apt-get install musl-dev musl-tools
 sudo apt-get install gcc-aarch64-linux-gnu
 sudo apt-get install gcc-mips64el-linux-gnuabi64
+sudo apt-get install gcc-s390x-linux-gnu
