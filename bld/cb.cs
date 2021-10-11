9f304bffba16b5cba38ac5b821c29c7fffbbca4c
dave <dave@torutek.com>
2021-10-11 14:01:12 +1300

    Build musl arm (armhf) too

--- bld/cb.cs
+++ bld/cb.cs
@@ -174,6 +174,10 @@ public static class cb
 					tw.Write(" -m64\n");
 					break;
 
+				case "musl-armhf":
+					compiler = "arm-linux-musleabihf-cc";
+					break;
+
 				default:
 					throw new NotImplementedException();
 			}
@@ -1210,6 +1214,7 @@ public static class cb
 			var targets_cross = new linux_target[]
 			{
 				new linux_target("musl-x64"),
+				new linux_target("musl-armhf"),
 				new linux_target("arm64"),
 				new linux_target("armhf"),
 				new linux_target("armsf"),
@@ -1900,6 +1905,7 @@ public static class cb
 			var targets_cross = new linux_target[]
 			{
 				new linux_target("musl-x64"),
+				new linux_target("musl-armhf"),
 				new linux_target("arm64"),
 				new linux_target("armhf"),
 				new linux_target("armsf"),
